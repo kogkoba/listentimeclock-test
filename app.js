04016b1d014b9242d84fc77e3efdfbdd88da9ab6
kogkoba <kc.koba@gmail.com>
2026-04-08 21:10:12 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("LISTEN Time Clock start");
---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  APP_NAME: "LISTEN Time Clock",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxPSBSdOnEUR9hUGRbzuj5w2yzarghDIiJkyFDh8hHUVeai1oOEorZ_Ge_VCG5rCOoR/exec"
+};
